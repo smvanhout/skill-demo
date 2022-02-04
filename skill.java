75941d04129134e897f171a09e3a58399e06f3da
smvanhout <86027836+smvanhout@users.noreply.github.com>
2022-02-04 15:22:41 -0800

    Lib and Starter

---
+++ skill.java
@@ -0,0 +1,5 @@
+public class skill{
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+}
---
+++ skillTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillTest {
+    @Test
+	public void testMultiply(){
+		assertEquals(2, 1*1); // This will fail
+	}
+
+}
